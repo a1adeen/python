0a1f221c2766435d7314876712f0f2f42554522f
aladeen <divsri9211@gmail.com>
2023-05-17 23:36:30 +0530

    completed d-5

---
+++ vd d-5.py
@@ -0,0 +1,8 @@
+# to seperate from a line 
+# \n
+
+print ("hey i am aladeen \n i am jod")
+# to put in quotation  (\"\") and for single quote (\'\')
+print("hey \n i am \"aladeen\" hi")
+print ("hey",7,6 , sep="_", end="bhai hai\n")
+print("i am aladeen \n i am god")
