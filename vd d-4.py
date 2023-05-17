ed305a718d1d0a84fb4f51484fd50bfcf5d3b3f4
aladeen <divsri9211@gmail.com>
2023-05-17 22:40:27 +0530

    did day4 and did quiz

---
+++ vd d-4.py
@@ -0,0 +1,8 @@
+print("helllo world")
+print("hi")
+print(7)
+print ("hie")
+print(17*3)
+
+# quiz here 
+print ("QuickSort algorithm to sort the elements. The pivot element is selected as the first element of the array, and the elements are partitioned into two lists (less and greater) based on their values relative to the pivot. The function then recursively calls itself on the less and greater lists, combining the sorted sublists with the pivot element in between. Finally, the sorted array is returned and printed.")
