40dac1a216c8e113546cffbbda1e267e78c506fe
aladeen <divsri9211@gmail.com>
2023-05-18 10:30:47 +0530

    comleted  vd-6

--- vd d-6.py
+++ vd d-6.py
@@ -2,3 +2,27 @@ a = 2
 print(a)
 b = "hey"
 print(b)
+
+# "" is used for strings
+
+a = 9 
+b = True
+c = "aladeen"
+print (" this is", type(a))
+print("this is " ,  type(b))
+print("this is" , type(c))
+
+# true is used for yes 
+# false is used for no in boolean 
+
+#mutation mainly means the changes in data strcture
+#immutalble which mainly means that can't be changed in data structure
+
+data1 = {
+    "name": "aladeen" , "age" : "17" ,
+    "elgible" : True
+}
+print(data1)
+
+
+#in python everything is object
