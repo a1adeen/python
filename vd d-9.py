dbdb650c1923a2b63ca5ecf5ce1c3b9929d831c9
aladeen <divsri9211@gmail.com>
2023-05-18 11:40:58 +0530

    did d-9

---
+++ vd d-9.py
@@ -0,0 +1,29 @@
+# typecasting
+
+# integer
+a = 1
+b = 2
+print( a + b ) 
+
+# output will be 3
+
+#string
+
+a = "1"
+b = "2"
+print( a + b)
+ #here output will be 12
+
+
+ # to add numbers in string
+
+a = "1"
+b = "3"
+c = "9"
+print ( int(a) + int(b) + int(c) )
+
+# implicit num = 1.22
+
+a = 2
+b = 5.8
+print( a + b)
