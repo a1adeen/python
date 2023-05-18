5c042ad8135c56357047a47acf99710fcf8a76a2
aladeen <divsri9211@gmail.com>
2023-05-18 11:14:56 +0530

    made calculatior

--- vd d-7,8.py
+++ vd d-7,8.py
@@ -4,3 +4,13 @@
 # ** refers to power 
 
 # making calculator
+
+a = 20 
+b = 2
+print(" the value of ", "a", "+" ,"b","is :" , a + b)
+print(" the value of ", "a", "+" ,"b","is :" , a / b)
+print(" the value of ", "a", "+" ,"b","is :" , a * b)
+print(" the value of ", "a", "+" ,"b","is :" , a - b)
+print(" the value of ", "a", "+" ,"b","is :" , a ** b)
+
+# print("hey ** 5")
