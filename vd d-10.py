ac64cb9e42711a126816a6491b3b913086a35447
aladeen <divsri9211@gmail.com>
2023-05-18 12:06:29 +0530

    did d-10

---
+++ vd d-10.py
@@ -0,0 +1,18 @@
+#taking input from user
+
+a = input("your ame here")
+print("hey i am", a)
+
+
+# adding 2 var 
+
+a = input("first name :")
+b = input("last name : ")
+
+print (  " my name is ",a + b)
+
+# adding non string var
+
+a = input()
+b = input()
+print(int(a) + int(b))
