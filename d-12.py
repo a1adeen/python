983000404ba97ee8669632aab95a0874634b4c58
aladeen <divsri9211@gmail.com>
2023-05-19 21:41:49 +0530

    completed d-12

---
+++ d-12.py
@@ -0,0 +1,12 @@
+name = ("ishant,aladeen")
+# for specific words
+print(name[7:11])
+# for whole
+print(name[:])
+print(name[:4])
+#quiz quest
+name = "harry"
+print(name[-4:-2])
+
+#answer should be har
+#its wrong ans qill be ar
