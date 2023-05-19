c93079d13b31982f5bc38a4ecdc2ca2f5c7c83ea
aladeen <divsri9211@gmail.com>
2023-05-19 16:14:20 +0530

    did day 11 11 and learned abhout array and index character

---
+++ vd d-11/to mak loops .py
@@ -0,0 +1,24 @@
+
+ishant = "aladeen"
+clusyyy = "anshu"
+hrsit = "spouk"
+
+aladeen = (' aladeen and im an"programer"')
+
+print( "hrymy name is" + aladeen)
+
+i = input("who are you:")
+print("hey my name is " + i)
+
+# '''''' (triple uqote)is mainly used to type mutli lines in string
+multiLineString = '''
+bhai hai 
+aladeen bhai don'''
+
+print(multiLineString)
+
+
+# array =  collection of items
+print("lets make a loop here \n")
+for character in multiLineString:
+    print(character)
