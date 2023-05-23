2382df125f96d3be0754a7852b1e9fe6068a7b35
aladeen <divsri9211@gmail.com>
2023-05-23 21:49:03 +0530

    did if and else today

---
+++ d-14.py
@@ -0,0 +1,47 @@
+# if and else statement
+
+a = int(input("enter ur age ="))
+print("your age is :",a)
+if (a>19):
+    print("you can drive")
+else:
+    print("you can't drive")
+
+# thats space before the print is called indentation     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+b = (input("your age :"))
+print("it's ur age",b)
+
+
+
+
+
