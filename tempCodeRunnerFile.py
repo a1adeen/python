43dc54f6b71a4df1737bbe5fc17827d3e60d92b4
aladeen <divsri9211@gmail.com>
2023-05-28 13:12:29 +0530

    comleted vd -19

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,25 @@
-    name = input("enter your name :") 
+# continue and break statement
+
+
+
+# # for loop 
+
+# for x in range(0 , 11):
+#     print(" 5 x ",x +1 , "=" , 5* (x + 1))
+#     if( x == 6):
+#         break
+
+
+
+
+# # 7 table
+# for i in range(10):
+#    print(" 7 X",i +1, "=", 7  * (i + 1) )
+
+
+# for it in range (15):
+#     if ( it == 10):
+#         continue
+#     print("5 X", it , "=", 5 * i)
+# print("it ends here")    
+
