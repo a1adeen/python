d533c9e2c48dbfbe6e946325634e411dc43cea64
aladeen <divsri9211@gmail.com>
2023-05-30 00:17:27 +0530

    did def and return fucntion

--- vd -20.py
+++ vd -20.py
@@ -37,3 +37,53 @@ calculatemean( d , h)
 
 
 
+# from brocode
+
+
+def happy_birthday(name):
+    print(f"hayyp birthday to {name} ; D")
+    print(f"hayyp birthday to {name} ; D")
+    print(f"hayyp birthday to {name} ; D")
+
+
+happy_birthday("aladeen")    
+
+# another example
+
+def happy_bday(naam , age):
+    print(f"happy birthday to {naam}")
+    print(f"your are {age} old")
+    print(f"haoppy birhtday to {naam}")
+
+happy_bday("ishant" , "9")    
+
+
+# example of invoice
+
+def invoice( username , amount , dueDate):
+    print(f"hey {username}")
+    print(f"your amount of ${amount} is due on :{dueDate}")
+
+
+invoice("aladeen" , 90900909, "01/12/2024")   
+
+
+
+# return function
+
+def add( c, v):
+    z = c + v
+    return z
+
+print(add( 2, 8))
+
+
+# first and last name function
+def your_name(first , last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + last
+
+fullName = your_name("aladeen", "man")
+
+print(fullName)
