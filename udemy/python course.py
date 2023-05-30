102d2c8ec80495fa55fa6052b5c61dac502b9448
aladeen <divsri9211@gmail.com>
2023-05-30 10:40:37 +0530

    data types

---
+++ udemy/python course.py
@@ -0,0 +1,11 @@
+# data type
+
+
+# integers =        int      ex - 2      5      6 292
+# floating point = float     ex- 2.5    0.5     6.9
+# string =          str      ex -"hey"  "yo"    "j hvkj"
+# lists  =          list     ex- ["hey ", 27 , 10.4]
+# dictionaries =    dict     ex- {"pass":"value","your name": "name"}
+# tuples =          tup      ex - ("hey ", 27 , 10.4)
+# stes =            set      ex- {"a","b"}
+# booleans =        bool     ex- true and false
