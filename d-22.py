6b18b57203c0764c553104140d8a9925870c5d11
aladeen <divsri9211@gmail.com>
2023-05-31 23:18:22 +0530

    did d-22 learned about lists

---
+++ d-22.py
@@ -0,0 +1,29 @@
+# list
+l = [2,254,52,25,5,252,5]
+print(l)
+print(type(l))
+
+# a = 'hey'
+# print(a.split())
+
+o = [3,3,3,3,3]
+if 89 in o:
+    print("yes")
+else:
+    ("no")    
+
+
+# -ve indexing
+lit = [4567,77,8,88,8,88,88]
+print(lit[1:-4])
+
+
+# making list using for loop
+lits = [i for i in range(10)]
+print(lits)
+
+
+#! adding some if statements
+
+pit = [ i for i in range(110) if i%2==0]
+print(pit)
