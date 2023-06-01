81c2624c6b13248a6e84d7ebf1ead3e6140e1738
aladeen <divsri9211@gmail.com>
2023-06-02 00:08:32 +0530

    comleted day 23

---
+++ d-23.py
@@ -0,0 +1,39 @@
+# apppend
+
+# TO ADD NO. IN LIST AT THE END
+list = [1,2,2,33,4,44,4]
+list.append(98)
+
+
+# TO make list in decending order
+list.sort(reverse=True)
+
+# to reverse the list
+list.reverse()
+print(list)
+
+
+# to make copy of the list
+
+hery = list.copy()
+print(list)
+
+
+# to iinsert a element on the specific index
+hey = [ 2,4,6,8,89]
+hey.insert(1 , 90)
+print(hey)
+
+
+# to add an items form 1 list to another
+i = [1,3,5,3,2,13]
+my = [22,5,444]
+i.extend(my)
+print(i)
+
+
+# to make a list by adding 2 list together 
+new_list = [2,23,54,534,5353]
+lyst = [ 23,45,5636,643]
+k = new_list + lyst
+print(k)
