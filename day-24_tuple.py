195d506b6f7955860e57d8153bef5281d4e322d0
aladeen <divsri9211@gmail.com>
2023-06-02 00:36:13 +0530

    made .py file for day-24 and revised fucntions

---
+++ day-24_tuple.py
@@ -0,0 +1,24 @@
+# def calculate_mean(a ,b):
+#     mean = ((a*b)/(a +b))
+#     if(mean == 0):
+#         print("numbers are invalid")
+#     elif(mean<10):
+#         print("mean is under 10")
+#     else:
+#         print(f"your mean is {mean}")
+#     print(mean)
+# calculate_mean(8, 20)
+
+
+# # invoice_function 
+
+# def invoice_function(name , due, date):
+#     print(f"{name} your due of {due}will expire on {date}")
+
+# invoice_function("ishnat", 900, 1092091)    
+
+
+
+
+
+# !tuples starts from here
