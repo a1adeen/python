25929349165bedeb144e8d9260dacb0ca6638f25
aladeen <divsri9211@gmail.com>
2023-06-03 13:22:11 +0530

    completed sol.2

--- ex-2.py
+++ ex-2.py
@@ -1,5 +1,19 @@
 # programme that says gm , good afternoon and  good night according to time zone
 import time
 
-# invite = (input("are you in"))
-# if(invite > )
+
+samay = time.strptime('%H:%M:%S')
+hours = int(time.strftime('%H'))
+print(samay)
+
+
+# if conditions
+if (samay>0  & samay<12):
+    print("goood morning")
+elif(samay > 12 & samay > 18 ):
+    print('good afternoon')
+    if(samay > 18 & samay <20):
+        print("good evenuing")
+    else:
+        print("good night")
+    
---
+++ exercise 3.py
@@ -0,0 +1,6 @@
+# KBC
+ 
+
+questions = 
+for i in questions():
+    print(i)
