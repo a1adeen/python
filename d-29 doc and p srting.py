bebc0bd99e488ebc5a31d20e27baf61247da588b
aladeen <divsri9211@gmail.com>
2023-06-06 23:44:37 +0530

    did doc string

---
+++ d-29 doc and p srting.py
@@ -0,0 +1,13 @@
+# doc String
+
+# to define the function 
+# thats how we can describe a function 
+#!print({name of the variable}__doc__)
+
+def square(n):
+    '''this function outputs the suqare of the input'''
+    print(n**2)
+square(9)    
+print(square.__doc__)
+
+
