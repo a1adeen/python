3ed36f2f6a90780d3cda6a2d4e1fd2e00000d99d
aladeen <divsri9211@gmail.com>
2023-06-09 10:53:09 +0530

    did d 31

---
+++ d-31 sets.py
@@ -0,0 +1,16 @@
+# sets 
+# seta are the unorderd collection of data and it do not repeat a same data type and it can't be changed
+
+seet = { 2,4,1,1,14}
+print(seet)
+
+
+# empty set 
+# to make an empty set 
+harry = set()
+print(type(harry))
+
+noot = {"hey", 1 , False, 22} 
+
+for i in noot:
+    print(i)
