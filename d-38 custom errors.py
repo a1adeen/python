b4cf165d9d3576162953c8ee7594d66476755715
a1adeen <divsri9211@gmail.com>
2023-06-15 01:13:35 +0530

    did value erors and did quiz to

---
+++ d-38 custom errors.py
@@ -0,0 +1,17 @@
+# # custom error
+# a = int(input("enter the value b/w 1 to 5 :" ))
+
+# if (a>=5 or a>=1):
+#     raise ValueError("value should be b/w 1 and 5")
+
+# if we use value error then if there will be any error then code will pause here and will not be executed 
+
+
+hey = (input("write 'quit' here :"))
+if(hey == "quit"):
+    print(hey)
+else:
+    raise ValueError("this can't be exepted")
+
+
+# did this : D 
