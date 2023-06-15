ae93b4a84bf65b301b42b2fee5bb759569640f4b
a1adeen <divsri9211@gmail.com>
2023-06-15 22:47:55 +0530

    completed d-42

---
+++ d-42.py
@@ -0,0 +1,19 @@
+# enumerate function 
+marks = [1,2,3,24,455,90]
+for index, mark in enumerate(marks):
+    print(mark)
+    if (index == 4):
+        print("aladeen jod !")
+
+
+# for dicitonaries
+# dic = {
+#     "hiten" : "10",
+#     "aladeen":"99",
+#     "gayman":"89",
+# } 
+# for index,dict in enumerate(dic):
+#     print(f"{index} got {dict.key()}")
+#     if (index == 1):
+#         print("aladeen jod")
+# !fck this code
