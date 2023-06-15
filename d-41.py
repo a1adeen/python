53f5d5724a75787d83d800a64233591f1076565c
a1adeen <divsri9211@gmail.com>
2023-06-15 22:54:39 +0530

    made changes in short hand if else

--- d-41.py
+++ d-41.py
@@ -3,3 +3,8 @@ a = 300
 b = 300
 print("A") if a > b else print("=") if a == b else print('B') 
 # this type of statement in if and else will be use only if the statement and short and can be defined in a line
+
+
+apple_qty = int(input("enter the amount of aapples  want :"))
+banana_qty = int(input("how much bananas u want :"))
+print("both are same in quantity ") if apple_qty == banana_qty else print("both have diff quantity") if apple_qty != banana_qty else print("teri mummi") 
