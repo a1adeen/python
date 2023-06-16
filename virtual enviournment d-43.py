3c7e788f804d65d22bbf4cf5d485b97b58e17e86
a1adeen <divsri9211@gmail.com>
2023-06-16 15:22:47 +0530

    did all the work in terminal

--- virtual enviournment d-43.py
+++ virtual enviournment d-43.py
@@ -1,1 +1,17 @@
 # virtual enviournment 
+import pandas as pd 
+print(pd.__version__)
+
+# did all this video work in terminal today i downladed virtual enviournment 
+# and learned how to set it up
+# virtualenv <name>
+# source <name>/bin/activate
+# to leave a env <deactivate>
+
+# to check all the version of the files that are downloaded
+# pip freeze 
+# to make a file of all the versions 
+# pip freeze > requirment.txt
+# and to download that in others pc 
+# pii install -r requiment.txt
+
