db5a06ef911fda2be40bb9afab26c653da866da0
a1adeen <divsri9211@gmail.com>
2023-06-18 13:54:18 +0530

    did os module

---
+++ d-46 od module.py
@@ -0,0 +1,27 @@
+# os module
+import os
+
+# to make directory 
+
+if not os.path.exists("new"):
+    os.mkdir("new")
+for i in range(1, 10):
+    os.mkdir(f"new/days{ i + 2}")
+
+
+# to change then name of folders in directory 
+for i in range(1 , 10 ):
+    os.rename(f"new/days{i + 2}", f"new/numers {i + 2}")
+
+to check how many folders are  theere in directtory 
+folders = os.listdir("new")
+print(folders)
+
+
+or 
+
+for folder in folders:
+    print(folder)
+
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,6 @@
-  # elif name == int(name):
-    #     print("no integers are allowed")
-    
+
+
+# if not os.path.exists("new"):
+#     os.mkdir("new")
+# for i in range(1, 10):
+#     os.mkdir(f"new/days{ i + 2}")
