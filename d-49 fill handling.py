d4aa16e5bdec8363414dafb0535de178a69ebdcb
a1adeen <divsri9211@gmail.com>
2023-06-26 10:58:54 +0530

    did file handling : read write append

---
+++ d-49 fill handling.py
@@ -0,0 +1,45 @@
+# # file handling
+
+
+
+
+
+# reading file 
+
+file = open('hey.txt', 'r')
+text = file.read()
+print(text)
+file.close()
+
+
+# to write a content in the file
+
+write = open('new.txt',  'w')
+write.write("hey it starts from here")
+write.close()
+
+
+
+# appending new txt in the file
+file = open('append.txt' , 'a')
+file.write("hey bhai hai")
+file.close()
+
+
+
+# ex of reading 
+
+# hi = open('append.txt' , 'r')
+# read = hi.read()
+# hi.close()
+# print(read)
+
+
+apppending = open('new.txt' , 'a')
+apppending.write("   : its the new text here")
+apppending.close()
+
+
+
+
+
