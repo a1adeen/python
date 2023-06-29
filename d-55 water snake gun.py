27c7a6b21ea89bc6fc043919f85bb5a7b839c4e9
a1adeen <divsri9211@gmail.com>
2023-06-29 19:04:30 +0530

    made changes in water snake gun game

--- d-55 water snake gun.py
+++ d-55 water snake gun.py
@@ -15,13 +15,17 @@ computer = {
 rules = ("before starting note that the -1 = snake   0 = water    and 1 = gun ")
 print(rules)
 start = (input("start playing :"))
-if start == 1:
+if (start == -1):
     print("you win")
 elif start == 0:
     print("you lost")
-if start == -1:
+if start == 1:
     print("u wins")
-print(start)
+elif(start == ""):
+        raise ValueError("no strings are allowed here")
+else:
+    print("teri mummi")
+
 
 
 # will continue this
