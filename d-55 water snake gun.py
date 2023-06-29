335c4f2aa31329e28fdef7c801b721d7138f49ca
a1adeen <divsri9211@gmail.com>
2023-06-29 11:03:52 +0530

    will continue this in evening

---
+++ d-55 water snake gun.py
@@ -0,0 +1,27 @@
+player = {
+    -1 : "snake",
+     0 : "water",
+     1 : "gun"
+
+}
+computer = {
+    2 : "snake",
+    3 : "water",
+    4 : "gun"
+}
+
+
+
+rules = ("before starting note that the -1 = snake   0 = water    and 1 = gun ")
+print(rules)
+start = (input("start playing :"))
+if start == 1:
+    print("you win")
+elif start == 0:
+    print("you lost")
+if start == -1:
+    print("u wins")
+print(start)
+
+
+# will continue this
