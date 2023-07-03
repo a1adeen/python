931840267ff94f8ee33790afb5c05ddc81034632
a1adeen <divsri9211@gmail.com>
2023-07-03 22:30:33 +0530

    did getters and setter

--- d-60 getter and setter.py
+++ d-60 getter and setter.py
@@ -16,3 +16,45 @@ h =My(1)
 h.how()
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Hey:
+    def __init__(self , k ):
+        self._kal = k
+
+    def show_me(self):
+        print(f"{self._kal} is the value ")
+
+    @property
+    def x_show_me(self):
+     return 10 * self._kal
+    @x_show_me.setter
+    def x_show_me(self , new):
+        self._new = self._kal / 10
+
+
+
+obj = Hey(100)
+obj.x_show_me = 10
+print(obj.x_show_me)
