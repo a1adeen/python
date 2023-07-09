0c86be87a0963641604baa86f273229ea26b0088
a1adeen <divsri9211@gmail.com>
2023-07-09 23:34:31 +0530

    made the nse file tommorow , will extract data from nse

---
+++ nse_data /nsepy.py
@@ -0,0 +1,9 @@
+# from nsepy import get_history
+# from datetime import date
+import nsepy
+from nsepy import get_history 
+data = data.get_history(symbol="sbin")
+print(data)
+
+
+# will do this tommorow 
