a7ecb7ff670ad57b9e274bf6f5509c5602dc5346
a1adeen <divsri9211@gmail.com>
2023-07-12 20:53:41 +0530

    did alternate class in class method

---
+++ d-70class_alternate.py
@@ -0,0 +1,53 @@
+# class method as alternative constructor
+class Employee:
+    def __init__(self , name , salary):
+        self.name = name
+        self.salary = salary
+        print(f"{self.name} earns {self.salary} from the company")
+        # if the info is give in string format
+    @classmethod
+    def another(cls,string):
+            return cls(string.split("-")[0], string.split("-")[1])
+
+        # this will work perfectly
+emp = Employee("aladeen", 1000020)
+# but if the data is given in string form then we will use alternate class method
+
+string = "aladesen-1202020"
+es = Employee.another(string)
+print(es.salary)
+hey = "ishant-101010"
+es_2  = Employee.another(hey)
+
+# class Employee:
+#     def __init__(self , name , salary):
+#         self.name = name
+#         self.salary = salary
+#
+#     @classmethod
+#     def from_str(cls , string):
+#         return cls(string.split("-")[0] , string.split("-")[1])
+#
+# e1 = Employee("aladdeen" , 120202)
+# print(e1.name)
+#
+# strin = "aladeen-12000"
+# e2 = Employee.from_str(strin)
+# print(e2.name)
+# print(e2.salary)
+#
+
+
+# using (,) for spilting
+class Player:
+    def __init__(self , naam , rank):
+        print("player class is activated")
+        self.naam = naam
+        self.rank = rank
+        print(f"{self,naam} is on {self.rank} in valorant")
+    @classmethod
+    def cust(pls , string):
+        return pls(string.split(",")[0] , string.split(",")[1])
+he = "aladeen,ace"
+es_8 = Player.cust(he)
+# print(es_8)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,17 +1,3 @@
 
-
-# # practising 
-# def welcome(fx):
-#     def mkj():
-#         print("good morning")
-#         fx()
-#         print("thanks for using this fuction")
-#     return mkj
-
-
-# @welcome
-# def name():
-#     Name = (input("whats your name :"))
-#     print(f"{Name} your name has been registered")
-
-# name()    
+# class Person:
+#     def __init__(self):
