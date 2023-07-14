cd2c4480c09ab2dd18ec8036b328d9baf82a09f0
a1adeen <divsri9211@gmail.com>
2023-07-14 23:07:55 +0530

    learnt about fe methhods today

---
+++ magic and dunder_method.py
@@ -0,0 +1,24 @@
+# magic and dunder method
+
+
+
+# magic method
+
+
+class Employeee:
+    def __init__(self , name ):
+        self.name = name
+    def __str__(self):
+        return f"name of the employee is {self.name}"
+    # this method will print if the the above method s commented
+    def __repr__(self):
+        return f"this mehtod is repr'previous mehtod is commented "
+    # this method is used when we need to call the class
+    def __call__(self):
+        print("he how are you")
+
+emo_1 = Employeee("aladeen")
+# print(emo_1)
+# # to call both mehtod at the same time
+# print(repr(emo_1))
+emo_1()
