0758be23d7edbd80799d16d902c27203ae600c6a
a1adeen <divsri9211@gmail.com>
2023-07-22 21:44:34 +0530

    learned about datetime moodule

---
+++ date_time.py
@@ -0,0 +1,12 @@
+import datetime
+
+print(dir(datetime))
+
+my_time = datetime.time()
+print(my_time)
+today_1 = datetime.date.today()
+print(today_1)
+
+
+
+# mainly we can have time and date info here
