979ab5cdeb911eb2e8d3be9e5e1a010d7c2fdeac
a1adeen <divsri9211@gmail.com>
2023-07-22 19:57:16 +0530

    tried collection module and used counter from that

---
+++ practise_code.py
@@ -0,0 +1,15 @@
+
+# here i'm importing modules and trying them'
+
+
+
+
+
+import collections
+from collections import Counter
+
+
+list_1 = [1,1,1,1,1,1,3,3,3,3]
+print(Counter(list_1))
+list_2 = ["hhe" , "hhe" , "hhe"  ,9,9,99]
+print(Counter(list_2))
