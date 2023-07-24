d40a2eb71675fe5055c09489e364639f59eddae5
a1adeen <divsri9211@gmail.com>
2023-07-24 13:02:47 +0530

    made programme to give shout out

--- ex9.py
+++ ex9.py
@@ -1,6 +1,9 @@
 # function to give shout out to ppl
 from os import system
 
-name = ['ishant' , "aladeen"]
-for i in name:
-    system(f"say shout out to {name}")
+# name = ['ishant' , "aladeen"]
+# for i in name:
+#     system(f"say shout out to {name}")
+#
+
+f
