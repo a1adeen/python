44e0dcb6c15edb607a7c7e8815c1550bab503200
a1adeen <divsri9211@gmail.com>
2023-07-26 00:06:21 +0530

    learned how to make make file extension of the code with get request

--- d-89.py
+++ d-89.py
@@ -1,7 +1,20 @@
 # request module
 import requests
+# from bs4 import BeautifulSoup
 
 # print(dir(requests))
 
-response = requests.get("https://github.com/a1adeen")
-print(response.text)
+# to get the code of the website
+# response = requests.get("https://github.com/a1adeen")
+# print(response.text)
+
+# to scrap a website
+# scrap = it means to exract the data from the website
+
+# with this we can make file of the required code
+url = "https://www.instagram.com/"
+r = requests.get(url)
+print(r.text)
+
+with open("insta.html" ,"w") as i:
+    i.write(r.text)
