9993bde44dba57e0cf94b2a73db31eb915bf67e9
a1adeen <divsri9211@gmail.com>
2023-08-02 22:26:03 +0530

    did multiprocessing

---
+++ d-98.py
@@ -0,0 +1,15 @@
+# multiprocessing
+import multiprocessing
+import requests
+#
+# def down(url , name ):
+#     response = requests.get(url)
+#     open(f"{name}.jpg" , "wb").write(response.content)
+#
+#
+# url = "https://picsum.photos/200/300"
+# for i in range(4):
+#     down(url , i )
+
+
+def counter(num):
