c8df00332c9f8bd87ff19dad808c9761c4e0d69c
a1adeen <divsri9211@gmail.com>
2023-08-08 23:34:23 +0530

    made deck class  and compared 2 cards with a value

--- milesotne_project.py
+++ milesotne_project.py
@@ -3,12 +3,8 @@ import random
 
 
 
-suit = {
-    0: 'Clubs',
-    1: 'Diamonds',
-    2: 'Hearts',
-    3: 'Spades'
-}
+suits = ( 'Clubs','Diamonds','Hearts','Spades')
+ranks = ("ace" , "two" , "three" , "four" , "five" , "six" , "seven" ,"eight" , "nine" , "ten" , "jack" , "king" , "queen")
 values = {
     1: 'Ace',
     2: '2',
@@ -38,13 +34,35 @@ class card:
 # comparing here
 card_1 = card("clubs" , 3)
 card_2 = card("hearts" , 7)
+card_3 = card("spades" , 13)
+card_4 = card("clubs" , 9)
+
+
 
 if (card_1.value < card_2.value):
     print("card_2 is greater than the card_1 ")
 else:
     print("card_1 is greater than the card_2")
+
+
+# other comparision
+if card_3.value == card_4.value:
+    print("true")
+else:
+    print("false")
 # print(card_1.value)
 
+# deck class
+class Deck:
+    def __init__(self):
+        self.all_cards = []
 
+        for suit in suits:
+            for rank in ranks:
 
+                createrd_crads = (suit , rank)
 
+                self.all_cards.append(createrd_crads)
+
+# new_deck = Deck()
+# print(new_deck.all_cards[-1])
