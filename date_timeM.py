d6813c7958ef62534d016803dff499021ed342bd
a1adeen <divsri9211@gmail.com>
2023-08-16 21:25:31 +0530

    learned about datetime module

---
+++ date_timeM.py
@@ -0,0 +1,12 @@
+import datetime
+
+#
+# my_time = datetime.time(13, 56 , 8)
+# print(my_time)
+# to check the current date
+today = datetime.date.today()
+print(today)
+
+# to check the current time
+timee = datetime.datetime.now()
+print(timee)
