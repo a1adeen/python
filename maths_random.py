fe3066b868cc88c1b03dbe909d298e756a5bbbb7
a1adeen <divsri9211@gmail.com>
2023-08-17 13:02:45 +0530

    learned about maths module

---
+++ maths_random.py
@@ -0,0 +1,15 @@
+# import random
+import math
+
+
+# print(dir(math))
+
+value = 3.4
+# to get the nearest value
+print(math.floor(value))
+# to get the preceeding value
+print(math.ceil(value))
+# inbuilding funvtion for floor
+print(round(value))
+
+
