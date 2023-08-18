88c339d0b5f8055adcba0a0373b438c29b641a90
a1adeen <divsri9211@gmail.com>
2023-08-18 19:17:56 +0530

    learned about debugger

---
+++ py_debugger.py
@@ -0,0 +1,20 @@
+import pdb
+
+
+def add():
+    i = 3
+    j = 4
+    print( i + j)
+x = 5
+y = 5
+z = 9
+result_2 = x + y
+result_1 = y + z
+print("starts from here : ")
+# pdb.set_trace()
+print(result_1)
+# breakpoint()
+print(result_2)
+# breakpoint()
+add()
+print("done")
