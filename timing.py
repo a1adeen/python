c10eb9c9e0b4575e95a8cc8449698c076b76853b
a1adeen <divsri9211@gmail.com>
2023-08-22 23:16:59 +0530

    learned how to get the time taken by fucntion to complete

---
+++ timing.py
@@ -0,0 +1,30 @@
+import time
+
+def func_1(n):
+    new_list = []
+    for num in range(n):
+        new_list.append((num))
+    return new_list
+#
+hey = func_1(10)
+print(hey)
+
+
+def func_2(m):
+    return list(map(str, range(m)))
+
+hey_2 = func_2(10)
+print(hey_2)
+
+
+# thats how we can calculate how time is taken by the compilar to give the output
+# current_time
+start_time = time.time()
+# running code
+result = func_1(1000000)
+print(result)
+# time after the execution of the code
+end_time = time.time()
+# time taken to execute
+eplapsed_time = start_time - end_time
+print(eplapsed_time)
