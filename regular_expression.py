e040010de135d410e703b0c8c4ea09831613dbe6
a1adeen <divsri9211@gmail.com>
2023-08-22 22:30:31 +0530

    completed the  re module

--- regular_expression.py
+++ regular_expression.py
@@ -23,3 +23,38 @@ print(hunt)
 
 hunt_2 = re.findall('\d{3}-\d{3}-\d{3}' , my_text)
 print(hunt_2)
+
+
+sear = 'hey how can i help u bhai hat sat cat '
+# by using | we can use or statment here
+hunt_3 = re.search('bhai|bhen' , sear)
+# and here we use dot for the strings who have the similar suffix
+hunt_4 = re.findall('.at' , sear)
+print(hunt_4)
+print(hunt_3)
+# this only searches the text which have the number at he start
+search_1 = '1 is a number'
+hunt_5 = re.findall('^\d' , search_1)
+print(hunt_5)
+# for the number at the end
+search_2 = 'the number is 2'
+hunt_6 = re.findall('\d$', search_2)
+print(hunt_6)
+
+
+
+
+
+
+# too exclude the number or text
+search_2 = 'there are 3! number?? 34 inside the text'
+# byt using the + sign we can get he texts together
+hunt_7 = r'[^\d]+'
+# to remove the specific words
+hunt_8 = r'[^!?]+'
+print(re.findall(hunt_8 , search_2))
+
+search_3 = 'hey my-name is ishant hello ala-deen'
+pattern = r'[\w]+-[\w]+'
+print(re.findall(pattern , search_3))
+
