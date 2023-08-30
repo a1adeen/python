54bd6f23cdb741e669890f7ac8e15d17a311dd32
a1adeen <divsri9211@gmail.com>
2023-08-30 12:07:58 +0530

    made changes in project

--- scraping_ex.py
+++ scraping_ex.py
@@ -18,11 +18,26 @@ import requests
 list_bucket = ["love","inspirational","life ","humor","book", "reading","freindship", "friends", "truth","simile"]
 for i in list_bucket:
       print(i , end=" , ")
-def fun_1(C):
-    return f'http://quotes.toscrape.com/tag/{C}/'
-print(fun_1("simile"))
 
-url = requests.get('http://quotes.toscrape.com/tag/simile/')
+# to get the 1st and 2nd page link of the tag
+def fun_1(C):
+    page_1 =  f'http://quotes.toscrape.com/tag/{C}/'
+    page_2 = f'http://quotes.toscrape.com/tag/{C}/page/2/'
+    print(page_1)
+    return page_2
+# function to check the author_name
+
+# def author(tag):
+
+
+    # if f"https://quotes.toscrape.com/tag/{C}/page/2/" :
+    #     return f'https://quotes.toscrape.com/tag/{C}/page/2/'
+    # else:
+    #     print('only one page is there ')
+print(fun_1("humor"))
+
+url = requests.get('http://quotes.toscrape.com/tag/simile/'
+                   '')
 # print(url.text)
 soup = bs4.BeautifulSoup(url.text , 'lxml')
 print(soup.select('.author')[0:])
