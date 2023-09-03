496b0d75a448af51c67a7be55cf129352d85fc8f
a1adeen <divsri9211@gmail.com>
2023-09-03 14:07:35 +0530

    completed half

--- p.py
+++ p.py
@@ -1,8 +1,12 @@
 import re
 
-st = 'print only the words that start with letter s in this sentence'
-for letter in st.split():
-    if letter[0] == 's':
-        print(letter)
-        
+num = int(input("num here"))
+if num >= 1:
+    for i in range(2 , num):
+        if num % i == 0:
+            print("its not prime number")
+            break
+    else:
+        print('its prime number ')
+
 
