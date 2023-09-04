3eebefd596093bea6d5a617f0b96322b7b7e7e9e
a1adeen <divsri9211@gmail.com>
2023-09-04 19:53:23 +0530

    did pythoon porblems

--- practise_code.py
+++ practise_code.py
@@ -1,15 +1,29 @@
 
-# here i'm importing modules and trying them'
+# # here i'm importing modules and trying them'
+#
+#
+#
+#
+#
+# import collections
+# from collections import Counter
+#
+#
+# list_1 = [1,1,1,1,1,1,3,3,3,3]
+# print(Counter(list_1))
+# list_2 = ["hhe" , "hhe" , "hhe"  ,9,9,99]
+# print(Counter(list_2))
 
 
+n = int(input("Enter an integer: "))
+print(n)
 
+if n % 2 != 0:
+    print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:
+    print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:
+    print("Weird")
+elif n % 2 == 0 and n > 20:
+    print("Not Weird")
 
-
-import collections
-from collections import Counter
-
-
-list_1 = [1,1,1,1,1,1,3,3,3,3]
-print(Counter(list_1))
-list_2 = ["hhe" , "hhe" , "hhe"  ,9,9,99]
-print(Counter(list_2))
