c0c1936aeedd9d075572d57b9b87675c72a8df03
a1adeen <divsri9211@gmail.com>
2023-09-07 00:07:01 +0530

    revised class methds

--- classes.py
+++ classes.py
@@ -11,7 +11,7 @@ class Dog:
         for i in range(num):
            print(f'woof my name is {self.name}')
 hey = Dog(breed='hiten' , name="hiten ")
-print(hey)
+# print(hey)
 
 class Circle():
     pi = 3.14
@@ -26,3 +26,74 @@ class Circle():
 
 num = Circle(3)
 print(num.area())
+
+# using inheritance
+
+class Animal():
+    def __init__(self):
+        print('animal created')
+
+    def who_am_i(self):
+        print('i am an animal')
+
+    def eat(self):
+        print('i am eating')
+
+
+
+# print(hey_1)
+
+
+# POLYMORPHISM
+
+
+# in this thsi the 2 diffrent class uses the same function name
+class Dog(Animal):
+    def __init__(self, name):
+        Animal.__init__(self)
+        self.name = name
+        print('Dog created')
+
+    def speak(self):
+        return f'{self.name} says woof'
+
+
+beti_dog = Dog(name="huuihiui")
+
+class Cat():
+    def __init__(self , name ):
+        Animal.__init__(self)
+        self.name = name
+        print('cat created')
+
+    def speak(self):
+        return f'{self.name} says moew'
+# naming the cat
+niko_the_cat = Cat(name = "sui")
+print(niko_the_cat)
+
+# function to for speaking of the animal
+def spk(nam):
+    print(nam.speak())
+
+
+
+class ANimal_2():
+    def __init__(self , naam):
+        self.naam = naam
+
+    def says(self):
+        raise NotImplementedError('subclass is need ')
+
+
+class Kuuta(ANimal_2):
+    def says(self):
+        return f'{self.naam} says woof!'
+
+class Billi(ANimal_2):
+    def says(self):
+        return f'{self.naam} says meow!!'
+
+
+kutt = Kuuta(naam='hey')
+print(kutt.says())
