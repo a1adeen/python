1ca74d7661af85a71cc8ed59849873213527c127
a1adeen <divsri9211@gmail.com>
2023-09-08 19:21:27 +0530

    comleted  exercise 1

--- class_hw.py
+++ class_hw.py
@@ -8,24 +8,34 @@ class Line():
         self.y_2 = y_2
 
 
-    # def distance(self):
-    #     return ((self.x_2 - self.x_1)**2 + (self.y_2 - self.y_1)**2)**1/2
+    def distance(self):
+        return ((self.x_2 - self.x_1)**2 + (self.y_2 - self.y_1)**2)**0.5
 
     def slope(self):
         return (self.y_2 - self.y_1) / (self.x_2 - self.x_1)
 
 
-cords = Line(3 , 2,8, 10)
+cords = Line(3,2,8,10)
 print(cords.slope())
 
 
 
 
 class Cylinder():
+    pi = 3.14
     def __init__(self , h , r):
         self.h = h
         self.r = r
     def volumn(self):
-        pass
+        return Cylinder.pi * self.r**2 * self.h
+
     def surfcae_area(self):
-        pass
+        return 2*Cylinder.pi * self.r * self.h + 2 * Cylinder.pi * self.r**2
+
+
+cyl = Cylinder(2 , 3)
+print(cyl.surfcae_area())
+
+
+
+
