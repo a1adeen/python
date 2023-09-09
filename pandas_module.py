afa0bdba5cb40c1166d9b28cc684c6eecae4c22d
a1adeen <divsri9211@gmail.com>
2023-09-09 23:10:23 +0530

    made csv file

--- pandas_module.py
+++ pandas_module.py
@@ -2,8 +2,12 @@ import pandas as pd
 import numpy as np
 
 dict_1 = {
-
+    "name" : ['harry' , 'rohan' , 'subh'],
+    'marks': [100 , 78 , 11],
+    'city' : [ 'rampur' , 'delhi' , 'kanpur']
 }
 
 
-
+frame = pd.DataFrame(dict_1)
+print(frame)
+frame.to_csv('data.csv')
