46b00052d0e49810090f607b56d7f77cdccc447d
a1adeen <divsri9211@gmail.com>
2023-09-13 15:58:16 +0530

    made weather pogramme

--- projects/prject_2.py
+++ projects/prject_2.py
@@ -3,13 +3,15 @@ import os
 import requests
 import json
 
-
-city = input('enter the name of the city')
-url = f'https://api.weatherapi.com/v1/current.json?key=1b24d4111cd64152af3100221231309&q={city}'
-
-r = requests.get(url)
-# print(r.text)
-dict = json.loads(r.text)
-temp_c = dict['current']['temp_c']
-print(temp_c)
-os.system(f'say {temp_c}')
+while True:
+    city = input('enter the name of the city')
+    url = f'https://api.weatherapi.com/v1/current.json?key=1b24d4111cd64152af3100221231309&q={city}'
+    if city == 'quit':
+        print('ggs')
+        break
+    r = requests.get(url)
+    # print(r.text)
+    dict = json.loads(r.text)
+    temp_c = dict['current']['temp_c']
+    print(temp_c)
+    os.system(f'say {temp_c}')
