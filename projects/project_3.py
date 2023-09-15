05cf4fd381459a9a71921d56a32e392c31baa9e8
a1adeen <divsri9211@gmail.com>
2023-09-15 12:36:56 +0530

    made a programme to shortern the picture

---
+++ projects/project_3.py
@@ -0,0 +1,25 @@
+# IMAGE resizer
+
+import cv2
+
+
+image = cv2.imread("book.jpg" , cv2.IMREAD_UNCHANGED)
+cv2.imshow("title", image)
+# cv2.waitKey(0)
+
+scale_perc = 50
+
+# to calculate the 50% percent of the orignal dimension
+new_widht = int(image.shape[1] * scale_perc / 100)
+new_height = int(image.shape[0] * scale_perc / 100)
+
+
+# desize
+# desize (new_widht , new_heightheight)
+
+
+# resize
+
+output= cv2.resize(image , (new_widht , new_height))
+cv2.imwrite('new_imag.jpg' , output)
+cv2.waitKey(0)
