ebf7105f6d4a85fccddc2c9edce6d14399e3f48b
a1adeen <divsri9211@gmail.com>
2023-09-22 22:58:13 +0530

    made function to replace a word from the string

--- projects/project_5.py
+++ projects/project_5.py
@@ -1,1 +1,65 @@
-# Facial recognition
+# word_replacment programme
+
+# function to replace the required word
+
+
+n = input('enter the strinig here :')
+
+def repl(n):
+
+
+    word_to_replace = input('enter the word to replace :')
+    word_for_replacemet = input('enter the word for replacement :')
+    return n.replace(word_to_replace , word_for_replacemet)
+
+
+hey = repl('hey my name is ishant')
+print(hey)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def name(nam):
+    word_to_replace = input('enter the word u want to replace :')
+    word_for_reoplacement = input('enter the replacement word :')
+    return nam.replace(word_to_replace , word_for_reoplacement)
+
+
+
+hey = name('aur bhai kesa hai tera name kya hai')
+print(hey)
