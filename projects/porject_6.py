02d53c4df6ffd7c86072d5639baf5c3bfb96849e
a1adeen <divsri9211@gmail.com>
2023-09-22 23:26:43 +0530

    will continue

---
+++ projects/porject_6.py
@@ -0,0 +1,12 @@
+# basic calculator
+
+
+def add(a , b):
+    return f'{a} + {b} = {a + b}'
+def multiply(a , b):
+    return f'{a} X {b} = {a * b}'
+def subtract(a , b):
+    return f'{a} - {b} = {a - b}'
+def devide(a , b ):
+    return f' {a} / {b} = { a / b}'
+
