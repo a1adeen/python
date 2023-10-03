c4108c70da6cbef633266a357b0880c1d3c353c1
a1adeen <divsri9211@gmail.com>
2023-10-03 14:59:58 +0530

    today i will make new programme

--- web-scrapping/scrapping.py
+++ web-scrapping/scrapping.py
@@ -2,25 +2,4 @@ import bs4
 from bs4 import BeautifulSoup
 import requests
 
-
-
-url = "https://www.codewithharry.com"
-content_url = requests.get(url)
-htmlcontent = content_url.content
-
-# print(htmlcontent
-
-# to parse the html content
-# in order
-soup = BeautifulSoup(htmlcontent , 'html.parser')
-# print(soup.prettify())
-
-# to get the tag
-#
-title= soup.li
-print(type(title))
-
-
-# to get all the elements for the tag
-
-
+# programme that tells the news
