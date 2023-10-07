32b6bc3fde6c0bc2a88795398bf76f7a8114e603
a1adeen <divsri9211@gmail.com>
2023-10-07 11:54:57 +0530

    gsg did few questions

---
+++ 10days/day 1/practise.py
@@ -0,0 +1,11 @@
+
+first_name = input("")
+print(first_name)
+last_name = input("")
+print(last_name)
+
+def print_full_name(first_name, last_name):
+    return f"Hello {first_name} {last_name} You just delved into python."
+
+hey = print_full_name("aladeen" , "man")
+print(hey)
